3a1fa6f38bc49fbd610eb14afc4ca7e2d04ffc75
Abongile21 <abongiletheo00@gmail.com>
2024-06-03 15:54:16 +0200

    added logic to models and controllers

--- models/rentalModel.js
+++ models/rentalModel.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const rentalSchema = new mongoose.Schema({
-  customerName: { type: String, required: true },
-  rentalStartDate: { type: Date, required: true },
-  rentalAmount: { type: Number, required: true }
+  customerName: { 
+    type: String,
+    required: true 
+},
+  rentalStartDate: { 
+    type: Date, 
+    required: true 
+},
+  rentalEndDate: {
+    type: Date, 
+    required: true
+  },
+  rentalAmount: { 
+    type: Number, 
+    required: true 
+}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Rental', rentalSchema);
