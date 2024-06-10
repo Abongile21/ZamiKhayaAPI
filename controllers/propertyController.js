19acc6e5f9b6f045b3e0a4769638695c6609c0e9
Abongile21 <abongiletheo00@gmail.com>
2024-06-10 13:17:35 +0200

    modified and added multer

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,9 +1,12 @@
 const Property = require('../models/Property');
 
-
 exports.createProperty = async (req, res) => {
     try {
-        const property = new Property(req.body);
+        const propertyData = req.body;
+        if (req.files) {
+            propertyData.images = req.files.map(file => file.path);
+        }
+        const property = new Property(propertyData);
         await property.save();
         res.status(201).json(property);
     } catch (error) {
@@ -11,7 +14,7 @@ exports.createProperty = async (req, res) => {
     }
 };
 
-xports.getAllProperties = async (req, res) => {
+exports.getAllProperties = async (req, res) => {
     try {
         const properties = await Property.find();
         res.status(200).json(properties);
@@ -19,6 +22,7 @@ xports.getAllProperties = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
 exports.getPropertyById = async (req, res) => {
     try {
         const property = await Property.findById(req.params.id);
@@ -31,10 +35,13 @@ exports.getPropertyById = async (req, res) => {
     }
 };
 
-// Update a property
 exports.updateProperty = async (req, res) => {
     try {
-        const property = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const propertyData = req.body;
+        if (req.files) {
+            propertyData.images = req.files.map(file => file.path);
+        }
+        const property = await Property.findByIdAndUpdate(req.params.id, propertyData, { new: true, runValidators: true });
         if (!property) {
             return res.status(404).json({ message: 'Property not found' });
         }
@@ -44,7 +51,6 @@ exports.updateProperty = async (req, res) => {
     }
 };
 
-// Delete a property
 exports.deleteProperty = async (req, res) => {
     try {
         const property = await Property.findByIdAndDelete(req.params.id);
--- routes/property.routes.js
+++ routes/property.routes.js
@@ -1,17 +1,12 @@
-// routes/propertyRoutes.js
 const express = require('express');
 const propertyController = require('../controllers/propertyController');
+const upload = require('../config/multerConfig');
 const router = express.Router();
 
-
-router.post('/properties', propertyController.createProperty)
-
+router.post('/properties', upload.array('images', 5), propertyController.createProperty);
 router.get('/properties', propertyController.getAllProperties);
-
 router.get('/properties/:id', propertyController.getPropertyById);
-
-router.put('/properties/:id', propertyController.updateProperty);
-
+router.put('/properties/:id', upload.array('images', 5), propertyController.updateProperty);
 router.delete('/properties/:id', propertyController.deleteProperty);
 
 module.exports = router;
